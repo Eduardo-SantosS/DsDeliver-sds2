588f1fd280630435a6e8108fe5cd91425a03dea2
Eduardo-SantosS <santos.eduardopro@gmail.com>
2021-01-11 23:48:40 -0300

    Finish front-mobile

--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./Home";
 import Orders from "./Orders";
+import OrderDetails from "./OrderDetails";
 
 const Stack = createStackNavigator();
 
@@ -14,11 +15,12 @@ export default function Routes() {
                 screenOptions={{
                     cardStyle: {
                         backgroundColor: '#fff'
-                    } 
+                    }
                 }}
             >
                 <Stack.Screen name="Home" component={Home}></Stack.Screen>
                 <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+                <Stack.Screen name="OrderDetails" component={OrderDetails}></Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     )
